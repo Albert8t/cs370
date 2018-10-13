a0c80a277a0a35efe8b14d063e3d2f7811a285fa
Cynthia Dong <cynthiadongwenqin@gmail.com>
2018-10-13 17:03:42 -0400

    pop up window

--- GoHousing/index.js
+++ GoHousing/index.js
@@ -1,25 +1,42 @@
-document.write("<script src=\"nodeModule/angular.js\"></script>");
-{
-    function hide(app) {
-        var app = angular.module("AngularshowhideApp", []);
-        app.controller('showhidectrl', function ($scope) {
+// document.write("<script src=\"nodeModule/angular.js\"></script>");
+// {
+//     function hide(app) {
+//         var app = angular.module("showhideApp", []);
+//         app.controller('showhidectrl', function ($scope) {
+//             $scope.showval = true;
+//             $scope.hideval = false;
+//             $scope.isShowHide = function (param) {
+//                 if (param == "show") {
+//                     $scope.showval = true;
+//                     $scope.hideval = false;
+//                 }
+//                 else if (param == "hide") {
+//                     $scope.showval = false;
+//                     $scope.hideval = true;
+//                 }
+//                 else {
+//                     $scope.showval = true;
+//                     $scope.hideval = true;
+//                 }
+//             }
+//         });
+//
+//     }
+// }
+
+
+var app = angular.module("showhideApp", [])
+.controller("showhidectrl", function ($scope) {
+    $scope.showval = false;
+    $scope.hideval = false;
+    $scope.isShowHide = function (param) {
+        if (param == "show") {
             $scope.showval = true;
             $scope.hideval = false;
-            $scope.isShowHide = function (param) {
-                if (param == "show") {
-                    $scope.showval = true;
-                    $scope.hideval = true;
-                }
-                else if (param == "hide") {
-                    $scope.showval = false;
-                    $scope.hideval = false;
-                }
-                else {
-                    $scope.showval = true;
-                    $scope.hideval = false;
-                }
-            }
-        });
-
+        }
+        else if (param == "hide") {
+            $scope.showval = true;
+            $scope.hideval = true;
+        }
     }
-}
+});
