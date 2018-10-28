2db929ca0c817bd5fe169ca41e402523299709f8
DESKTOP-KKISTSR\ALIENWARE <albert.li@emory.edu>
2018-10-28 15:47:01 -0400

    signup pofile

--- GoHousing/profile.php
+++ GoHousing/profile.php
@@ -6,13 +6,13 @@
  * Time: 16:38
  */
 print"complete";
-session_start();
+$id=$_POST['id'];
 $profile['FirstName']=$_POST['FirstName'];
 $profile['LastName']=$_POST['LastName'];
 $profile['school']=$_POST['uni'];
 $profile['gender']=$_POST['Gender'];
 $profile['year']=$_POST['Year'];
-$id=$_SESSION['id'];
+
 echo($profile['FirstName']);
 echo('<br>');
 echo($profile['LastName']);
@@ -23,8 +23,35 @@ echo($profile['gender']);
 echo('<br>');
 echo($profile['year']);
 echo('<br>');
-echo($id);
+echo($profile['id']);
 echo('<br>');
+
+$mysqli = new mysqli("localhost", "root", "practicum370", "GoHousin");
+
+if (mysqli_connect_errno())            # ------ check connection error
+{
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit(1);
+}
+
+        $sex=3;
+if($profile['gender']==='male')
+    $sex=1;
+if($profile['gender']==='female')
+
+
+    $id=intval($id);
+$insert="insert into profile (uid,fname,lname,school, gender,year)  values(".$id.",'". $profile['FirstName']."','". $profile['LastName']."','".$profile['school']."',".$sex.",'".$profile['year']."')";
+if ($mysqli->query($insert) === TRUE) {
+    echo "Your account has been set up";
+} else {
+    echo "Error: " . $insert . "<br>" . $mysqli->error;
+}
+
+echo('inserted');
+
+$mysqli->close();
+
 
 
 ?>
