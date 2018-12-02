7ddd8a431e7f621cf5b124c133a6d92992487181
Cynthia Dong <cynthiadongwenqin@gmail.com>
2018-12-01 21:27:29 -0500

    profile

--- GoHousing/js/main.js
+++ GoHousing/js/main.js
@@ -1,140 +1,272 @@
-/* =================================
-------------------------------------
-	Civic - CV Resume
-	Version: 1.0
- ------------------------------------ 
- ====================================*/
+ AOS.init({
+ 	duration: 800,
+ 	easing: 'slide'
+ });
+
+$(document).ready(function($) {
+
+	"use strict";
+
+	$(window).stellar({
+    responsive: false,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: 'scroll'
+  });
 
 
+	// loader
+	var loader = function() {
+		setTimeout(function() { 
+			if($('#ftco-loader').length > 0) {
+				$('#ftco-loader').removeClass('show');
+			}
+		}, 1);
+	};
+	loader();
 
-'use strict';
+	var carousel = function() {
+		$('.carousel').owlCarousel({
+			loop: true,
+			margin: 10,
+			nav: true,
+			stagePadding: 5,
+			nav: false,
+			navText: ['<span class="icon-chevron-left">', '<span class="icon-chevron-right">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 2
+				},
+				1000:{
+					items: 3
+				}
+			}
+		});
+
+		$('.nonloop-block-13').owlCarousel({
+	    center: false,
+	    items: 1,
+	    loop: false,
+			stagePadding: 0,
+	    margin: 20,
+	    nav: true,
+			navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+	    responsive:{
+        600:{
+        	margin: 20,
+          items: 2
+        },
+        1000:{
+        	margin: 20,
+          items: 2
+        },
+        1200:{
+        	margin: 20,
+          items: 3
+        }
+	    }
+		});
+
+		$('.loop-block-31').owlCarousel({
+			loop: true,
+			margin: 0,
+			nav: true,
+			items: 1,
+			autoplay: true,
+			stagePadding: 0,
+			nav: true,
+			navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+			animateOut: 'fadeOut',
+    	animateIn: 'fadeIn',
+		});
+
+		$('.nonloop').owlCarousel({
+	    center: true,
+	    items:2,
+	    loop:false,
+	    margin:10,
+	    nav: true,
+			navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+	    responsive:{
+        600:{
+          items:2
+        }
+	    }
+		});
+	};
+	carousel();
+
+	// scroll
+	var scrollWindow = function() {
+		$(window).scroll(function(){
+			var $w = $(this),
+					st = $w.scrollTop(),
+					navbar = $('.ftco_navbar'),
+					sd = $('.js-scroll-wrap');
+
+			if (st > 150) {
+				if ( !navbar.hasClass('scrolled') ) {
+					navbar.addClass('scrolled');	
+				}
+			} 
+			if (st < 150) {
+				if ( navbar.hasClass('scrolled') ) {
+					navbar.removeClass('scrolled sleep');
+				}
+			} 
+			if ( st > 350 ) {
+				if ( !navbar.hasClass('awake') ) {
+					navbar.addClass('awake');	
+				}
+				
+				if(sd.length > 0) {
+					sd.addClass('sleep');
+				}
+			}
+			if ( st < 350 ) {
+				if ( navbar.hasClass('awake') ) {
+					navbar.removeClass('awake');
+					navbar.addClass('sleep');
+				}
+				if(sd.length > 0) {
+					sd.removeClass('sleep');
+				}
+			}
+		});
+	};
+	scrollWindow();
+
+	var counter = function() {
+		
+		$('#section-counter').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+
+				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+				$('.ftco-number').each(function(){
+					var $this = $(this),
+						num = $this.data('number');
+						console.log(num);
+					$this.animateNumber(
+					  {
+					    number: num,
+					    numberStep: comma_separator_number_step
+					  }, 7000
+					);
+				});
+				
+			}
+
+		} , { offset: '95%' } );
+
+	}
+	counter();
+	
+	
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.ftco-animate').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .ftco-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn ftco-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft ftco-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight ftco-animated');
+							} else {
+								el.addClass('fadeInUp ftco-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 50, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '95%' } );
+	};
+	contentWayPoint();
+
+	// navigation
+	var OnePageNav = function() {
+		$(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function(e) {
+		 	e.preventDefault();
+
+		 	var hash = this.hash,
+		 			navToggler = $('.navbar-toggler');
+		 	$('html, body').animate({
+		    scrollTop: $(hash).offset().top
+		  }, 700, 'easeInOutExpo', function(){
+		    window.location.hash = hash;
+		  });
 
 
-$(window).on('load', function() { 
-	/*------------------
-		Preloder
-	--------------------*/
-	$(".loader").fadeOut(); 
-	$("#preloder").delay(400).fadeOut("slow");
+		  if ( navToggler.is(':visible') ) {
+		  	navToggler.click();
+		  }
+		});
+		$('body').on('activate.bs.scrollspy', function () {
+		  console.log('nice');
+		})
+	};
+	OnePageNav();
+
+
+	// magnific popup
+	$('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+     gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+
+  $('#checkin_date, #checkout_date').datepicker({
+	  'format': 'd MM, yyyy',
+	  'autoclose': true
+	});
+
+
 
 });
-
-
-(function($) {
-
-	/*------------------
-		Background set
-	--------------------*/
-	$('.set-bg').each(function() {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
-
-
-	$('.review-slider').owlCarousel({
-		loop: true,
-		nav: false,
-		dots: true,
-		items: 1,
-		autoplay: true
-	});
-
-
-
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
-		}
-	});
-
-
-	$('.lan-prog').each(function() {
-		var progress = $(this).data("lanprogesss");
-		var ele      = '<span></span>';
-		var ele_fade = '<span class="fade-ele"></span>';
-		
-		for (var i = 1; i <= 5; i++) {
-			if(i <= progress){
-				$(this).append(ele);
-			} else {
-				$(this).append(ele_fade);
-			}
-		}
-	});
-
-
-	/*------------------
-		Popup
-	--------------------*/
-	$('.portfolio-item .port-pic').magnificPopup({
-		type: 'image',
-		mainClass: 'img-popup-warp',
-		removalDelay: 500,
-	});
-
-
-
-
-if($().circleProgress){
-
-	//Set progress circle 1
-	$("#progress1").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 2
-	$("#progress2").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle white
-	$("#progress3").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#ffffff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle white
-	$("#progress4").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#ffffff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle skyblue
-	$("#progress5").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#009fff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle skyblue
-	$("#progress6").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#009fff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-}
-
-})(jQuery);
 
--- GoHousing/profile.php
+++ GoHousing/profile.php
@@ -4,51 +4,48 @@
     <title>Go Housin</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
-    <!--<link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">-->
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <!--<link rel="stylesheet" href="css/magnific-popup.css">-->
+    <link rel="stylesheet" href="css/magnific-popup.css">
     <link rel="stylesheet" href="css/aos.css">
     <link rel="stylesheet" href="css/ionicons.min.css">
-    <!--<link rel="stylesheet" href="css/bootstrap-datepicker.css">-->
-    <!--<link rel="stylesheet" href="css/jquery.timepicker.css">-->
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
     <link rel="stylesheet" href="css/flaticon.css">
     <link rel="stylesheet" href="css/icomoon.css">
     <link rel="stylesheet" href="css/style.css">
-
-
-
-
 </head>
+<body>
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
-        <a class="navbar-brand" href="../../index.html">GoHousin</a>
+        <a class="navbar-brand" href="index.html">GoHousin</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="oi oi-menu"></span> Menu
         </button>
 
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a href="http://www.gohousin.com/app/information%20review/GablesReview.php" class="nav-link">Information Reivew</a></li>
-                <li class="nav-item"><a href="../information%20sharing/information%20sharing.html" class="nav-link">Information Sharing</a></li>
-                <li class="nav-item"><a href="../roommate%20searching/roommate%20searching.html" class="nav-link">Roommate Searching</a></li>
-                <li class="nav-item"><a href="../aboutus/about.html" class="nav-link">About Us</a></li>
-                <li class="nav-item"><a href="../contact/contact.php" class="nav-link">Contact Us</a></li>
+                <li class="nav-item"><a href="app/information%20review/information%20review.html" class="nav-link">Information Reivew</a></li>
+                <li class="nav-item"><a href="app/information%20sharing/information%20sharing.html" class="nav-link">Information Sharing</a></li>
+                <li class="nav-item"><a href="app/roommate%20searching/roommate%20searching.html" class="nav-link">Roommate Searching</a></li>
+                <li class="nav-item"><a href="app/aboutus/about.html" class="nav-link">About Us</a></li>
+                <li class="nav-item"><a href="app/contact/contact.php" class="nav-link">Contact Us</a></li>
             </ul>
         </div>
     </div>
 </nav>
-<body>
+
+
 	<?php
     session_start();
 		if (!empty($_POST['FirstName'])) {
 		    $id = $_POST['id'];
 		    $email=$_POST['email'];
             $profile['FirstName']= $_POST['FirstName'];
-            $profile['LastNmae'] = $_POST['LastName'];
+            $profile['LastName'] = $_POST['LastName'];
             $profile['school'] = $_POST['uni'];
             $profile['gender'] = $_POST['Gender'];
             $profile['year'] = $_POST['Year'];
@@ -57,70 +54,88 @@
                 printf("Connect failed: %s\n", mysqli_connect_error());
                 exit(1);
             }
-        $sex = 3;
-        if ($profile['gender'] === 'male') {
-            $sex = 1;
-        }
-        if ($profile['gender'] === 'female') {
-            $sex = 2;
-        }
-        $id = intval($id);
+            $sex = 3;
+            if ($profile['gender'] === 'Male') {
+                $sex = 1;
+            }
+            if ($profile['gender'] === 'Female') {
+                $sex = 2;
+            }
+            $id = intval($id);
             $insert = "insert into profile (uid,fname,lname,school, gender,year)  values(" . $id . ",'" . $profile['FirstName'] . "','" . $profile['LastName'] . "','" . $profile['school'] . "'," . $sex . ",'" . $profile['year'] . "')";
-        if ($mysqli->query($insert) === TRUE) {
+            if ($mysqli->query($insert) === TRUE) {
+                $_SESSION['username']=$email;
+                $_SESSION['loggedin']=true;
+                echo('<br>');
+                echo($_SESSION['username']);
+                echo('<br>');
+                echo($_SESSION['loggedin']);
+                echo('<br>');
+     ?>
 
-            $_SESSION['username']=$email;
-            $_SESSION['loggedin']=true;
-
-            echo('<br>');
-            echo($_SESSION['username']);
-            echo('<br>');
-            echo($_SESSION['loggedin']);
-            echo('<br>');
-            ?>
-    <section class="hero-section spad">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-xl-10 offset-xl-1">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="hero-text">
-                                <h2>
-                                    <?php
-                                    echo $profile['FirstName'] . " " . $profile['LastName'];
-                                    ?>
-                                </h2>
-                                <p>Welcome to GoHousin</p>
-                            </div>
-                            <div class="hero-info">
-                                <ul>
-                                    <li><span>school</span>
-                                        <?php
-                                            echo $profile['school'];
-                                        ?>
-                                    </li>
-                                    <li><span>Year</span>
-                                        <?php
-                                            echo $profile['year'];
-                                        ?>
-                                    </li>
-                                    <li><span>Gender</span>
-                                        <?php
-                                            echo $profile['gender'];
-                                        ?>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <figure class="hero-image">
-                                <img src="img/hero.jpg" alt="5">
-                            </figure>
-                        </div>
-                    </div>
-                </div>
+<div class="block-30 block-30-sm item"  data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-10">
+                <span class="subheading-sm">Profile Page</span>
+                <h2 class="heading">Wenqin Dong</h2>
             </div>
         </div>
-    </section>
+    </div>
+</div>
+<br>
+<br>
+<!--    <section class="hero-section spad">-->
+<!--        <div class="container-fluid">-->
+<!--            <div class="row">-->
+<!--                <div class="col-xl-10 offset-xl-1">-->
+<!--                    <div class="row">-->
+<!--                        <div class="col-lg-6">-->
+<!--                            <div class="hero-text">-->
+<!--                                <h2>-->
+<!--                                    --><?php
+//                                    echo $profile['FirstName'] . " " . $profile['LastName'];
+//                                    ?>
+<!--                                </h2>-->
+<!--                                <p>Welcome to GoHousin</p>-->
+<!--                            </div>-->
+<!--                            <div class="hero-info">-->
+<!--                                <ul>-->
+<!--                                    <li><span>school</span>-->
+<!--                                        --><?php
+//                                            echo $profile['school'];
+//                                        ?>
+<!--                                    </li>-->
+<!--                                    <li><span>Year</span>-->
+<!--                                        --><?php
+//                                            echo $profile['year'];
+//                                        ?>
+<!--                                    </li>-->
+<!--                                    <li><span>Gender</span>-->
+<!--                                        --><?php
+//                                            echo $profile['gender'];
+//                                        ?>
+<!--                                    </li>-->
+<!--                                </ul>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                        <div class="col-lg-6">-->
+<!--                            <figure class="hero-image">-->
+<!--                                <img src="img/hero.jpg" alt="5">-->
+<!--                            </figure>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </section>-->
+
+<div class = "reviewtitle">
+    <div class = "reviewinput">
+        <h3>Here is your information</h3>
+    </div>
+</div>
+
             <?php
 	            } else {
 	                echo "insert error";
@@ -139,75 +154,75 @@
                 $ifExist = "select password,id, email from login where email='" . $login['email'] . "'";
                 $result = mysqli_query($mysqli, $ifExist);
                 if (mysqli_num_rows($result) == 0) {
-                    echo("No account yet");
-                    echo('<br>');
-                    print("<a class=\"navbar-brand\" href=\"http://gohousin.com/signup.php\">Sign Up for a New Account</a>");
-                    echo('<br>');
-                    print("OR Sign In To Your Account");
-                    echo('<br>');
-                    print("<div class = \"container-login\">
-        <form class = \"form-login\"  ACTION=\"http://www.gohousin.com/profile.php\" METHOD=\"POST\">
+                    echo "No account yet";
+                    echo "<br>";
+    //                 print "<a class=\"navbar-brand\" href=\"http://gohousin.com/signup.php\">Sign Up for a New Account</a>";
+    //                 echo('<br>');
+    //                 print("OR Sign In To Your Account");
+    //                 echo('<br>');
+    //                 print("<div class = \"container-login\">
+    //     <form class = \"form-login\"  ACTION=\"http://www.gohousin.com/profile.php\" METHOD=\"POST\">
 
-            <div class=\"dialog\">
-                <a href=\"index.html\" class=\"close-thick\"></a>
-            </div>
-            <br>
-            <br>
-            <input type=\"hidden\" id=\"up\" name=\"up\" value=true>
-            <div class= \"email\">
-                    <span class= \"txt1\">
-                        Email
-                    </span>
-            </div>
-            <div class=\"input-title\">
-                <input class = \"input1\" type=\"text\" name=\"email\" >
-            </div>
+    //         <div class=\"dialog\">
+    //             <a href=\"index.html\" class=\"close-thick\"></a>
+    //         </div>
+    //         <br>
+    //         <br>
+    //         <input type=\"hidden\" id=\"up\" name=\"up\" value=true>
+    //         <div class= \"email\">
+    //                 <span class= \"txt1\">
+    //                     Email
+    //                 </span>
+    //         </div>
+    //         <div class=\"input-title\">
+    //             <input class = \"input1\" type=\"text\" name=\"email\" >
+    //         </div>
 
-            <div class=\"password\">
-                    <span class=\"txt1\">
-							Password
-                    </span>
-                <a href=\"app/login/forgotpassword.html\" class=\"txt2\">
-                    Forgot?
-                </a>
-            </div>
-            <div class=\"input-title\">
-                <input class=\"input1\" type=\"password\" name=\"pass\" >
-            </div>
-            <br>
-            <br>
-            <div class=\"login-button\">
-                <button class=\"button-form\" ng-click=\"isShowHide('show')\">
-                    Sign In
-                </button>
-            </div>
-            <br>
-        </form>
-    </div>");
+    //         <div class=\"password\">
+    //                 <span class=\"txt1\">
+				// 			Password
+    //                 </span>
+    //             <a href=\"app/login/forgotpassword.html\" class=\"txt2\">
+    //                 Forgot?
+    //             </a>
+    //         </div>
+    //         <div class=\"input-title\">
+    //             <input class=\"input1\" type=\"password\" name=\"pass\" >
+    //         </div>
+    //         <br>
+    //         <br>
+    //         <div class=\"login-button\">
+    //             <button class=\"button-form\" ng-click=\"isShowHide('show')\">
+    //                 Sign In
+    //             </button>
+    //         </div>
+    //         <br>
+    //     </form>
+    // </div>");
 
                 }
-                $row = $result->fetch_assoc();
-                $hashed =  (string)$row['password'];
-                $signin = false;
+            //     $row = $result->fetch_assoc();
+            //     $hashed =  (string)$row['password'];
+            //     $signin = false;
 
-            $rehash=md5($pass);
+            // $rehash=md5($pass);
 
-            if ($rehash===$hashed) {
+            // if ($rehash===$hashed) {
 
-                echo('Login Success');
-                $signin = true;
-                $_SESSION['username']=$login['email'];
-                $_SESSION['loggedin']=true;
+            //     echo('Login Success');
+            //     $signin = true;
+            //     $_SESSION['username']=$login['email'];
+            //     $_SESSION['loggedin']=true;
 
-                echo('<br>');
-                print($_SESSION['username']);
-                echo('<br>');
-                print($_SESSION['loggedin']);
-                echo('<br>');
-            } else {
-                echo("incorrect password, try again");
-            }
-            echo('end');
+            //     echo('<br>');
+            //     print($_SESSION['username']);
+            //     echo('<br>');
+            //     print($_SESSION['loggedin']);
+            //     echo('<br>');
+            // } else {
+            //     echo("incorrect password, try again");
+            // }
+            // echo('end');
             $mysqli->close();
         }
 
@@ -234,4 +249,5 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
     <script src="js/google-map.js"></script>
     <script src="js/main.js"></script>
+</body>
 </html>
